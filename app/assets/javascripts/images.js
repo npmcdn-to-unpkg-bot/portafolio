29923b01041c7ee8a373dcee90da6c35c53db508
Andre <afre92@gmail.com>
2016-02-22 16:17:42 -0500

    masonry grid added and lightbox

--- app/assets/javascripts/images.js
+++ app/assets/javascripts/images.js
@@ -1,34 +1,67 @@
-function showImage(smSrc, lgSrc) {
-              document.getElementById('largeImg').src = smSrc;
-              showLargeImagePanel();
-              unselectAll();
-              setTimeout(function() {
-                  document.getElementById('largeImg').src = lgSrc;  }, 1)
-          }
-          function showLargeImagePanel() {
-              document.getElementById('largeImgPanel').style.display = 'block';
-          }
-          function unselectAll() {
-              if(document.selection)
-                  document.selection.empty();
-              if(window.getSelection)
-                  window.getSelection().removeAllRanges();
-          }
+var openGallery = function(){
+(function($) {
+    var $pswp = $('.pswp')[0];
+    var image = [];
+
+    $('.picture').each( function() {
+        var $pic     = $(this),
+            getItems = function() {
+                var items = [];
+                $pic.find('a').each(function() {
+                    var $href   = $(this).attr('href'),
+                        $size   = $(this).data('size').split('x'),
+                        $width  = $size[0],
+                        $height = $size[1];
+
+                    var item = {
+                        src : $href,
+                        w   : $width,
+                        h   : $height
+                    }
+
+                    items.push(item);
+                });
+                return items;
+            }
+
+        var items = getItems();
+
+        $.each(items, function(index, value) {
+            image[index]     = new Image();
+            image[index].src = value['src'];
+        });
+
+        $pic.on('click', 'img', function(event) {
+            event.preventDefault();
+
+            var $index = $(this).index();
+            var options = {
+                index: $index,
+                bgOpacity: 0.7,
+                showHideOpacity: true
+            }
+
+            var lightBox = new PhotoSwipe($pswp, PhotoSwipeUI_Default, items, options);
+            lightBox.init();
+        });
+    });
+})(jQuery);
+}
+var masonry = function() {
+
+  var container = document.querySelector('#masonry');
+    var msnry = new Masonry( container, {
+      columnWidth: '.item',
+      itemSelector: '.item'
+
+    });
+
+};
+
+
 $(window).load(function() {
-  function showImage(smSrc, lgSrc) {
-                document.getElementById('largeImg').src = smSrc;
-                showLargeImagePanel();
-                unselectAll();
-                setTimeout(function() {
-                    document.getElementById('largeImg').src = lgSrc;  }, 1)
-            }
-            function showLargeImagePanel() {
-                document.getElementById('largeImgPanel').style.display = 'block';
-            }
-            function unselectAll() {
-                if(document.selection)
-                    document.selection.empty();
-                if(window.getSelection)
-                    window.getSelection().removeAllRanges();
-            }
+  masonry();
+
+
+
 });
