e16931a6f91d9814d8802981371063fd52cd490e
Andre <afre92@gmail.com>
2016-02-29 11:03:14 -0500

    aws added

--- app/assets/javascripts/images.js
+++ app/assets/javascripts/images.js
@@ -1,52 +1,4 @@
-var openGallery = function(){
-(function($) {
-    var $pswp = $('.pswp')[0];
-    var image = [];
 
-    $('.picture').each( function() {
-        var $pic     = $(this),
-            getItems = function() {
-                var items = [];
-                $pic.find('a').each(function() {
-                    var $href   = $(this).attr('href'),
-                        $size   = $(this).data('size').split('x'),
-                        $width  = $size[0],
-                        $height = $size[1];
-
-                    var item = {
-                        src : $href,
-                        w   : $width,
-                        h   : $height
-                    }
-
-                    items.push(item);
-                });
-                return items;
-            }
-
-        var items = getItems();
-
-        $.each(items, function(index, value) {
-            image[index]     = new Image();
-            image[index].src = value['src'];
-        });
-
-        $pic.on('click', 'img', function(event) {
-            event.preventDefault();
-
-            var $index = $(this).index();
-            var options = {
-                index: $index,
-                bgOpacity: 0.7,
-                showHideOpacity: true
-            }
-
-            var lightBox = new PhotoSwipe($pswp, PhotoSwipeUI_Default, items, options);
-            lightBox.init();
-        });
-    });
-})(jQuery);
-}
 var masonry = function() {
 
   var container = document.querySelector('#masonry');
@@ -61,6 +13,7 @@ var masonry = function() {
 
 $(window).load(function() {
   masonry();
+
 
 
 
