cb39b7b1d1bb843fec8e4c8222830b763a0f628d
Andre <afre92@gmail.com>
2016-03-14 00:04:03 -0400

    waiting

--- app/assets/javascripts/images.js
+++ app/assets/javascripts/images.js
@@ -1,15 +1,38 @@
 
 
 
-var masonry = function() {
- var container = document.querySelector('#masonry');
-  var msnry = new Masonry( container, {
-    columnWidth: '.hov',
-    itemSelector: '.hov'
-  });
-};
+// var masonry = function() {
 
+// };
+
+
+
+$(document).ready(function() {
 
 $(window).load(function() {
-  masonry();
+  // masonry();
+  var container = document.querySelector('#masonry');
+   var msnry = new Masonry( container, {
+     columnWidth: '.hov',
+     itemSelector: '.hov'
+   });
+
+
+
+
+  // var $container = $('#masonry');
+// $container.imagesLoaded( function(){
+//     var msnry = new Masonry( $container[0], {
+//         columnWidth: '.hov',
+//         itemSelector: '.hov'
+//     });
+// });
+
+////////////////////////////////////
+// $container.imagesLoaded( function(){
+//         $container.masonry({
+//             columnWidth: '.hov',
+//             itemSelector: '.hov'
+//         });
+    });
 });
