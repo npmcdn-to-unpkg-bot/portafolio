85239f8daff059e5ff18256a9714da71c3d05ab2
Andre <afre92@gmail.com>
2016-03-15 17:54:08 -0400

    turbolinks fixed

--- app/controllers/images_controller.rb
+++ app/controllers/images_controller.rb
@@ -15,43 +15,43 @@ class ImagesController < ApplicationController
   end
 
 
-  # def edit
-  # end
+  def edit
+  end
 
-  # def create
-  #   @image = Image.new(image_params)
-  #
-  #   respond_to do |format|
-  #     if @image.save
-  #       format.html { redirect_to @image, notice: 'Image was successfully created.' }
-  #       format.json { render :show, status: :created, location: @image }
-  #     else
-  #       format.html { render :new }
-  #       format.json { render json: @image.errors, status: :unprocessable_entity }
-  #     end
-  #   end
-  # end
+  def create
+    @image = Image.new(image_params)
+
+    respond_to do |format|
+      if @image.save
+        format.html { redirect_to @image, notice: 'Image was successfully created.' }
+        format.json { render :show, status: :created, location: @image }
+      else
+        format.html { render :new }
+        format.json { render json: @image.errors, status: :unprocessable_entity }
+      end
+    end
+  end
 
 
-  # def update
-  #   respond_to do |format|
-  #     if @image.update(image_params)
-  #       format.html { redirect_to @image, notice: 'Image was successfully updated.' }
-  #       format.json { render :show, status: :ok, location: @image }
-  #     else
-  #       format.html { render :edit }
-  #       format.json { render json: @image.errors, status: :unprocessable_entity }
-  #     end
-  #   end
-  # end
+  def update
+    respond_to do |format|
+      if @image.update(image_params)
+        format.html { redirect_to @image, notice: 'Image was successfully updated.' }
+        format.json { render :show, status: :ok, location: @image }
+      else
+        format.html { render :edit }
+        format.json { render json: @image.errors, status: :unprocessable_entity }
+      end
+    end
+  end
 
-  #
-  # def destroy
-  #   @image.destroy
-  #   respond_to do |format|
-  #     format.html { redirect_to images_url, notice: 'Image was successfully destroyed.' }
-  #     format.json { head :no_content }
-  #   end
+
+  def destroy
+    @image.destroy
+    respond_to do |format|
+      format.html { redirect_to images_url, notice: 'Image was successfully destroyed.' }
+      format.json { head :no_content }
+    end
   # end
 
   private
