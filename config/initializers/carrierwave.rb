d7607c082d91f2d8aa2cf4b1c047d523891b6a7e
Andre <afre92@gmail.com>
2016-03-14 20:59:29 -0400

    changed aws name

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -2,10 +2,10 @@ require 'carrierwave'
 
 CarrierWave.configure do |config|
   config.fog_credentials = {
-    :provider               => 'AWS',       # required 
+    :provider               => 'AWS',       # required
     :aws_access_key_id      => ENV['ACCESS_KEY'], # required
     :aws_secret_access_key  => ENV['SECRET_KEY'] # required
   }
-  config.fog_directory  = ENV['AWS_BUCKET']                     # required
+  config.fog_directory  = 'trate'                     # required
 
 end
